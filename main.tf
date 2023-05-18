8191ba3dad78f131708feb23eccf345ca2710e2f
afzalepam <afzalbasha992@gmail.com>
2023-05-18 13:43:03 +0530

    changes

--- main.tf
+++ main.tf
@@ -1,5 +1,7 @@
 resource "aws_ssm_parameter" "parameter" {
-  name  = "test.test1"
-  type  = "String"
-  value = "Hello test1 lo unna"
+  count=length(var.parameters)
+  type  = "SecureString"
+  key_id = "2f11dea3-eec7-41ec-9c20-8089258d17c8"
+  name= var.parameters[count.index].name
+  value= var.parameters[count.index].value
 }
